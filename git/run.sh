41f250a69aa9488fc26470d2294a13a9d080b92c
kthieler <Only4github>
2016-07-17 13:04:32 -0400

    First Commit!

--- git/run.sh
+++ git/run.sh
@@ -3,14 +3,14 @@
 if [ -d "$TARGET_DIR" ]; then
     cd $TARGET_DIR
 else
-	echo "Error: Directory not found: $BUILD_DIR"
+	echo "Error: Directory not found: $TARGET_DIR"
 	exit 1
 fi
 
-if [ -e $GIT_REPO ]
+if [ ! -z  "$GIT_REPO" ]
 then
     git clone $GIT_REPO
 else
-	echo "Error: Git repo no defined. "
+	echo "Error: GIT_REPO no defined. "
 	exit 1
 fi
